ee35899f661035e11bd479302b29fa7f3cc03027
Tao <jt10y@icloud.com>
2019-05-04 12:54:06 -0700

    Update methods_and_main.py

--- methods_and_main.py
+++ methods_and_main.py
@@ -14,5 +14,17 @@ def find_user():
 
 	return results
 
+def submit_new(user_photos, user_fingerprint, user_voice):
+	user = user()
 
+	user.photo = user_photos
+	user.fingerprint = user_fingerprint
+	user.voice = user_voice
 
+	return results
+
+def submit_check(user_photos, user_fingerprint, user_voice):
+	user = user()
+
+	return user_photos == user.photo and user_fingerprint == user.fingerprint and
+			user_voice == user.voice
